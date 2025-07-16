ec53716f3c9de68b910130d26cc82d4535dc90f9
SrJaggy <jag8210@gmail.com>
2025-07-16 06:23:45 -0500

    new Function added

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -9,7 +9,7 @@ export default function MenuItem({ item, addItem }: MenuItemProps) {
     return (
         <button
             className=" border-2 border-teal-400 hover:bg-teal-200 p-3 text-lg  rounded-lg flex justify-between w-full"
-            onClick={() => addItem()}
+            onClick={() => addItem(item)}
         >
             <p>{item.name}</p>
             <p className="font-black">${item.price}</p>
